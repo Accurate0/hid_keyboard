bca7e6f81d31671f5939c5f0db200a2155b6788d
Anurag Singh <contact@anurag.sh>
2021-08-31 18:22:59 +0800

    update comment

--- qmk/common/hid_commands.h
+++ qmk/common/hid_commands.h
@@ -2,7 +2,7 @@
 
 enum HIDCommands
 {
-    // https://github.com/qmk/qmk_firmware/blob/master/quantum/via.c : 68
+    // https://github.com/qmk/qmk_firmware/blob/master/quantum/via.h#L68
     // skip VIA straight to my handler :)
     VIA_LIGHTING_SET_VALUE = 0x07,
     VOLUME_COMMAND = 0x0,
